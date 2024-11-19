487c40e3928987deb909740d18b802d1a0637e59
haruta <c0a23125b@edu.teu.ac.jp>
2024-11-19 16:56:43 +0900

    演習課題1

--- pygame_image.py
+++ pygame_image.py
@@ -29,12 +29,21 @@ def main():
         key_lst=pg.key.get_pressed()
         if key_lst[pg.K_UP]:
             kk_rect.move_ip((0,-1))
-        if key_lst[pg.K_LEFT]:
-            kk_rect.move_ip((-1,0))
         if key_lst[pg.K_DOWN]:
             kk_rect.move_ip((0,1))
+        if key_lst[pg.K_LEFT]:
+            kk_rect.move_ip((-1,0))
         if key_lst[pg.K_RIGHT]:
             kk_rect.move_ip((1,0))
+        else:
+            kk_rect.move_ip(-1,0)
+            
+
+
+
+
+
+        
 
 
         screen.blit(bg_img, [x, 0]) #blitで画面表示
