81ca3b15fb45fae18a312a7b254d4b22913535d5
haruta <c0a23125b@edu.teu.ac.jp>
2024-11-19 15:38:21 +0900

    練習2:こうかとん左右反転

--- pygame_image.py
+++ pygame_image.py
@@ -9,13 +9,17 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))#(横、縦)
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")#サーフェイス=bg_img
+    bg_img = pg.image.load("fig/pg_bg.jpg") #サーフェイス=bg_img
+    #こうかとん画像2.pngを読み込み、surfaceを生成
+    kk_img=pg.image.load("fig/2.png")
+    kk_img=pg.transform.flip(bg_img,True,False)
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])#blitで画面表示
+        screen.blit(bg_img, [0, 0]) #blitで画面表示
         pg.display.update()
         tmr += 1        
         clock.tick(10)#フレームレート
